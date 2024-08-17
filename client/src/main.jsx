992cf1bfd3a30cae507e362e5c4e0c077d8fcefd
Mayank Soni <mayank93soni@gmail.com>
2024-08-17 16:19:02 +0530

    add ngixn con again

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Final from './Final.jsx'
 import Apps from './App.jsx'
 createRoot(document.getElementById('root')).render(
 //   <StrictMode>
